f34b16f0e276646968958f35948c1963cf484055
Anudeep31 <81857643+Anudeep31@users.noreply.github.com>
2024-10-09 12:21:48 +0530

    Update app.js
    release 1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello anudeep Upgrad v2 :)'));
+app.get('/', (req, res) => res.send('Release1   ---Hello anudeep Upgrad v2 :)'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
